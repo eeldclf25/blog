866cf796639b9eec6ccc73c6d1ea84f71a39b921
SeongJong <eeldclf25@gmail.com>
2026-01-22 08:14:37 +0900

    feat: update footer and site icon

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ninari",
+    pageTitle: "Kim heu",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
